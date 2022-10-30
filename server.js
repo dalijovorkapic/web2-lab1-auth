812d5d735a5d7df02281c7a03f8ab6fcb473b437
dalijovorkapic <dv52809@fer.hr>
2022-10-30 11:06:36 +0100

    commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lab1'));
+app.use(express.static('./dist/lab1-auth'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/lab1/'}),
+    res.sendFile('index.html', {root: 'dist/lab1-auth'}),
 );
 
 // Start the app by listening on the default Heroku port
