baf331b46c027ce5495a6775e789d0047008393e
dalijovorkapic <dv52809@fer.hr>
2022-10-30 20:42:58 +0100

    finish

--- src/app/auth-initializer/auth-initializer.component.ts
+++ src/app/auth-initializer/auth-initializer.component.ts
@@ -13,6 +13,7 @@ import { komentarModel } from 'src/models/komentarModel';
 import { ljetstvicaModel } from 'src/models/ljestvicaModel';
 import { MatSort } from '@angular/material/sort';
 import { MatTab } from '@angular/material/tabs';
+import { delay, of } from 'rxjs';
 
 
 
@@ -29,12 +30,20 @@ export class AuthInitializerComponent implements OnInit {
   username: string = "";
   email: string = "";
   currentId: number = 0;
+  currentMatchId: number = 0;
+  currentKoloId: number = 0;
+  showEditScoreInput: boolean = false;
+
+  temp_tablica: ljetstvicaModel[] = [];
 
   newComment: boolean = false;
   editingComment: boolean = false;
   editCommentId: number = 0;
+  token_expiration: number = 0;
 
   commentValue: string = "";
+  score1: number;
+  score2: number;
   koloId: number = 0;
 
   tablica_poretka: ljetstvicaModel[] = [
@@ -52,7 +61,19 @@ export class AuthInitializerComponent implements OnInit {
 
   kola: koloModel[] = [
     {
-      id:1, naziv:'12. Kolo', odigrano:true, 
+      id:1, naziv:'11. Kolo', odigrano:true, 
+      utakmice_kola: [
+        {id:1, datum:'02.10.2022. 15:00', lokacija: 'HNK Rijeka', odigrana:true, protivnik1:'Rijeka', protivnik2:'Hajduk', rezultat:'0 : 1'},
+        {id:2, datum:'02.10.2022. 17:30', lokacija: 'Šubićeva, Šibenik', odigrana:true, protivnik1:'Šibenik', protivnik2:'Velika Gorica', rezultat:'1 : 1'},
+        {id:3, datum:'01.10.2022. 16:55', lokacija: 'Stadion gradski vrt, Osijek', odigrana:true, protivnik1:'Osijek', protivnik2:'Lokomotiva', rezultat:'4 : 1'},
+        {id:4, datum:'01.10.2022. 19:30', lokacija: 'Stadion Maksimir, Zagreb', odigrana:true, protivnik1:'Dinamo', protivnik2:'Slaven Belupo', rezultat:'4 : 1'},
+        {id:5, datum:'30.09.2022. 18:00', lokacija: 'Stadion Varteks, Varaždin', odigrana:true, protivnik1:'Varaždin', protivnik2:'Istra 1961', rezultat:'1 : 1'}
+      ],
+      komentari: [{id: 1, datum:'03.10.2022, 17:52:23', comment:'Kritična igra', user:'marko.markic', email:'marko.markic@gmail.com'},
+                  {id: 2, datum:'03.10.2022, 17:52:23', comment:'Lijepe riječi molim! :)', user:'admin', email:'admin@net.hr'}]
+    },
+    {
+      id:2, naziv:'12. Kolo', odigrano:true, 
       utakmice_kola: [
         {id:1, datum:'08.10.2022. 19:05', lokacija: 'Gradski stadion Poljud, Split', odigrana:true, protivnik1:'Hajduk', protivnik2:'Varaždin', rezultat:'2 : 1'},
         {id:2, datum:'08.10.2022. 17:10', lokacija: 'Aldo Drosina, Pula', odigrana:false, protivnik1:'Istra 1961', protivnik2:'Dinamo', rezultat:'- : -'},
@@ -60,10 +81,11 @@ export class AuthInitializerComponent implements OnInit {
         {id:4, datum:'07.10.2022. 18:00', lokacija: 'Nogometni stadion Zagreb, Zagreb', odigrana:true, protivnik1:'Lokomotiva', protivnik2:'Šibenik', rezultat:'1 : 1'},
         {id:5, datum:'09.10.2022. 17:10', lokacija: 'Gradski stadion, Velika Gorica', odigrana:true, protivnik1:'Velika Gorica', protivnik2:'Rijeka', rezultat:'0 : 2'}
       ],
-      komentari: [{id: 1, datum:'10/22/2022, 13:52:23', comment:'livajaaaa', user:'Marko', email:'marko@net.hr'}]
+      komentari: [{id: 1, datum:'09.10.2022, 14:12:23', comment:'Trofej ide u Rijeku D:', user:'bruno.brunic', email:'bruno.brunic@gmail.com'},
+                  {id: 2, datum:'10.10.2022, 18:52:23', comment:'@bruno.brunic kako da ne :P', user:'marko.markic', email:'marko.markic@gmail.com'}]
     },
     {
-      id:2, naziv:'13. Kolo', odigrano:true, 
+      id:3, naziv:'13. Kolo', odigrano:true, 
       utakmice_kola: [
         {id:1, datum:'15.10.2022. 19:05', lokacija: 'Gradski stadion, Velika Gorica', odigrana:true, protivnik1:'Velika Gorica', protivnik2:'Hajduk', rezultat:'0 : 1'},
         {id:2, datum:'16.10.2022. 17:10', lokacija: 'HNK Rijeka, Rijeka', odigrana:true, protivnik1:'Rijeka', protivnik2:'Lokomotiva', rezultat:'3 : 0'},
@@ -74,7 +96,7 @@ export class AuthInitializerComponent implements OnInit {
       komentari: []
     },
     {
-      id:3, naziv:'14. Kolo', odigrano:false, 
+      id:4, naziv:'14. Kolo', odigrano:false, 
       utakmice_kola: [
         {id:1, datum:'21.10.2022. 18:00', lokacija: 'Gradski stadion Poljud, Split', odigrana:false, protivnik1:'Hajduk', protivnik2:'Dinamo', rezultat:'- : -'},
         {id:2, datum:'22.10.2022. 17:10', lokacija: 'Stadion Varteks, Varaždin', odigrana:false, protivnik1:'Varaždin', protivnik2:'Osijek', rezultat:'- : -'},
@@ -100,26 +122,35 @@ export class AuthInitializerComponent implements OnInit {
             this.email = user.name || ""
           }
         })
-      }
 
-
-      if(authenticated) {
         let userRaw = localStorage.getItem(`@@auth0spajs@@::${environment.clientId}::default::openid profile email`)
         let user = JSON.parse(userRaw!)
         
         let id_token = user.body.id_token
-
+      
         let helper = new JwtHelperService()
 
         let decoded_id_token = helper.decodeToken(id_token)
 
         this.userRoles = decoded_id_token[`${environment.baseUrl}/roles`];
 
-        
+        this.token_expiration = user.body.expires_in
+
+        this.calculatePoints;
+        this.logoutTimer(this.token_expiration)
+
       }
     })
 
     this.calculatePoints();
+  }
+
+  logoutTimer(timeout:number) {
+    of(null).pipe(delay(timeout)).subscribe((expired) => {
+      console.log('EXPIRED!!');
+
+      this.auth.logout({ returnTo: document.location.origin })
+    })
   }
 
   calculatePoints() {
@@ -160,7 +191,7 @@ export class AuthInitializerComponent implements OnInit {
       klub.bodovi = (klub.broj_pobjeda*3)+(klub.broj_nerjesenih);
     })
     this.tablica_poretka.sort((a,b) => b.bodovi - a.bodovi)
-    console.log(this.tablica_poretka)
+
     for (let i=0; i<this.tablica_poretka.length; i++) {
       this.tablica_poretka[i].poredak = i+1;
     }
@@ -171,11 +202,17 @@ export class AuthInitializerComponent implements OnInit {
   addComment(id: number) {
     this.currentId = id;
     this.newComment = true;
+    this.commentValue = "";
   }
 
   onCommentSubmit() {
     if(this.editingComment) {
-      this.kola[this.koloId-1].komentari[this.editCommentId-1].comment = this.commentValue
+      this.kola[this.koloId-1].komentari.forEach(komentar => {
+        if(komentar.id == this.editCommentId) {
+          komentar.comment = this.commentValue
+        }
+      })
+      this.editingComment = false;
     }
     else {
       this.kola[this.koloId-1].komentari.push({
@@ -199,16 +236,71 @@ export class AuthInitializerComponent implements OnInit {
   }
 
   deleteComment(commentId: number, koloId: number) {
-    this.kola[koloId-1].komentari.splice(commentId-1, 1)
+    for(let i=0; i< this.kola[koloId-1].komentari.length; i++) {
+      if(commentId == this.kola[koloId-1].komentari[i].id){
+        this.kola[koloId-1].komentari.splice(i,1);
+      }
+    }
   }
 
-  editComment(commentId: number, comment:string) {
+  editComment(commentId: number, comment:string, kolo_id:number) {
+    this.editingComment = true;
+    this.currentId = kolo_id;
     this.editCommentId = commentId;
     this.commentValue = comment;
-    this.editingComment = true;
   }
 
   passInfo(id: number) {
+    //this.commentValue = "";
     this.koloId = id;
+  }
+
+  editScore(match_id: number, kolo_id: number) {
+    let rez_arr = this.kola[kolo_id-1].utakmice_kola[match_id-1].rezultat.split(" : ")
+    let rez1 = rez_arr[0] || 0
+    let rez2 = rez_arr[1] || 0
+    if(rez1 != "-" && rez2 != "-") {
+      this.score1 = Number(rez1)
+      this.score2 = Number(rez2)
+    }
+    else {
+      this.score1 = 0;
+      this.score2 = 0;
+    }
+    this.currentMatchId = match_id;
+    this.currentKoloId = kolo_id;
+    this.showEditScoreInput = true;
+  }
+
+  onScoreSubmit(kolo_id: number, match_id: number) {
+    this.tablica_poretka.forEach(klub => {
+      klub.bodovi = 0;
+      klub.ukupno_odigranih_utakmica = 0;
+      klub.broj_nerjesenih = 0;
+      klub.broj_pobjeda = 0;
+      klub.broj_poraza = 0;
+    })
+
+    this.kola[kolo_id-1].utakmice_kola[match_id-1].rezultat = this.score1+" : "+this.score2
+    this.score1 = 0;
+    this.score2 = 0;
+    this.showEditScoreInput = false;
+
+    this.calculatePoints();
+  }
+
+  cancelScoreEdit(kolo_id:number, match_id:number) {
+    this.showEditScoreInput = false;
+    let rez_arr = this.kola[kolo_id-1].utakmice_kola[match_id-1].rezultat.split(" : ")
+    let rez1 = rez_arr[0] || 0
+    let rez2 = rez_arr[1] || 0
+    if(rez1 != "-" && rez2 != "-") {
+      this.score1 = Number(rez1)
+      this.score2 = Number(rez2)
+    }
+    else {
+      this.score1 = 0;
+      this.score2 = 0;
+    }
   }
 }
