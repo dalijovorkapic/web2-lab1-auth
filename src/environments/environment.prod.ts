3d67d7684422a8530bd7f52aac6803c7ef2ca1e0
dalijovorkapic <dv52809@fer.hr>
2022-10-30 11:12:50 +0100

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:4200',
+  baseUrl: 'https://web2-lab1-auth.herokuapp.com',
   clientId: 'YvCGmFEldVLktNNAEA2D215SDbYCkv5N',
   domain: 'dev-wiwlh5g5.us.auth0.com'
 };
